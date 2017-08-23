0fbdb70ae9a03fc7711562a6e3e5f26dcb36f558
lucas8 <luc.linux@mailoo.org>
2017-08-23 15:14:49 +0200

    Add teensy loader to nix dependencies

--- keyboards/ergodox/ez/default.nix
+++ keyboards/ergodox/ez/default.nix
@@ -3,6 +3,7 @@ let
   inherit (nixpkgs) pkgs;
   packages = with pkgs; [ gcc gnumake unzip wget zip avrgcclibc
                           dfu-programmer dfu-util git
+                          teensy-loader-cli
                         ];
 in pkgs.stdenv.mkDerivation {
   name = "qmk_firmware";
